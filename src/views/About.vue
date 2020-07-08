c2fbd5883575880b73f44b4c1df34c879d161b64
zhutiantian <zhutiantian@RJB-ZHUTIANTIAN>
2020-07-08 16:51:13 +0800

    jj

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,14 +2,14 @@
   <div class="about">
     <h1>This is an about page</h1>
     <test @click="ccaa" :name="name" />
-    <input v-model="a" @change="ccaa" type="text" />
+    <input v-model="message" @change="ccaa" type="text" />
     {{message}}
   </div>
 </template>
 
 <script>
 import test from "./test.js";
-import { reactive, watch, ref, toRefs } from "vue";
+import { reactive, watchEffect, toRefs } from "vue";
 export default {
   components: {
     test
@@ -20,11 +20,9 @@ export default {
       message: "Hello World!!!",
       name: "name"
     });
-    let a = ref("");
-    watch(
-      a,
+    watchEffect(
       () => {
-        console.log("aa", a.value);
+        console.log("aa", state.message);
       },
       {
         lazy: true
@@ -32,14 +30,8 @@ export default {
     );
     const ccaa = () => {
       console.log(2222);
-      state.message = state.message
-        .split("")
-        .reverse()
-        .join("");
-      console.log("state.message: ", state.message);
     };
     return {
-      a,
       ccaa,
       ...toRefs(state)
     };
